4d9d19faeca62af55a45102e5de867ad51d2ab52
María Carrera <carreratalaveron@gmail.com>
2024-10-21 12:59:41 +0200

    fix: python lint requirements

--- app/modules/notepad/routes.py
+++ app/modules/notepad/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 from app.modules.notepad.forms import NotepadForm
@@ -10,6 +10,8 @@ notepad_service = NotepadService()
 '''
 READ ALL
 '''
+
+
 @notepad_bp.route('/notepad', methods=['GET'])
 @login_required
 def index():
@@ -21,6 +23,8 @@ def index():
 '''
 CREATE
 '''
+
+
 @notepad_bp.route('/notepad/create', methods=['GET', 'POST'])
 @login_required
 def create_notepad():
@@ -41,11 +45,13 @@ def create_notepad():
 '''
 READ BY ID
 '''
+
+
 @notepad_bp.route('/notepad/<int:notepad_id>', methods=['GET'])
 @login_required
 def get_notepad(notepad_id):
     notepad = notepad_service.get_or_404(notepad_id)
-    
+
     if notepad.user_id != current_user.id:
         flash('You are not authorized to view this notepad', 'error')
         return redirect(url_for('notepad.index'))
@@ -56,6 +62,8 @@ def get_notepad(notepad_id):
 '''
 EDIT
 '''
+
+
 @notepad_bp.route('/notepad/edit/<int:notepad_id>', methods=['GET', 'POST'])
 @login_required
 def edit_notepad(notepad_id):
@@ -85,6 +93,8 @@ def edit_notepad(notepad_id):
 '''
 DELETE
 '''
+
+
 @notepad_bp.route('/notepad/delete/<int:notepad_id>', methods=['POST'])
 @login_required
 def delete_notepad(notepad_id):
@@ -100,5 +110,3 @@ def delete_notepad(notepad_id):
         flash('Error deleting notepad', 'error')
     
     return redirect(url_for('notepad.index'))
-
-
